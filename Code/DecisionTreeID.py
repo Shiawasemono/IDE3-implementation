658b1647d73c6fa534643f1be940a0576477f8b0
Shiawasemono <nono.amps@gmail.com>
2018-10-24 19:00:22 +0200

    Añadido TODO

--- Code/DecisionTreeID.py
+++ Code/DecisionTreeID.py
@@ -1,14 +1,27 @@
+import pandas as pd
+import numpy as np
+
 def Entropia(matriz_datos,index=False,valor=0):
 	valDif = matriz_datos[matriz_datos.columns[-1]].value_counts()
-	l = len(data_matrix)
+	#TODO: pasar a df y trabajar
+	print(list(valDif))
+	adsgfasd
+	l = len(matriz_datos)
 	if not index:
 		#Estamos calculando la entropía de la variable de decisión
 		for k in valDif:
 			p = k[1]/l 
-			E += p*log2(p)
+			E += p*np.log2(p)
 	else:
-		cnt = matriz_datos.groupby([matriz_datos.columns[index],matriz_datos.columns[-1]).size()
-
-
-
+		cnt = matriz_datos.groupby([matriz_datos.columns[index],matriz_datos.columns[-1]]).size()
+		print(cnt)
 	return -E
+
+raw_data = {'regiment': ['Nighthawks', 'Nighthawks', 'Nighthawks', 'Nighthawks', 'Dragoons', 'Dragoons', 'Dragoons', 'Dragoons', 'Scouts', 'Scouts', 'Scouts', 'Scouts'], 
+        'company': ['1st', '1st', '2nd', '2nd', '1st', '1st', '2nd', '2nd','1st', '1st', '2nd', '2nd'], 
+        'name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze', 'Jacon', 'Ryaner', 'Sone', 'Sloan', 'Piger', 'Riani', 'Ali'], 
+        'preTestScore': [4, 24, 31, 2, 3, 4, 24, 31, 2, 3, 2, 3],
+        'postTestScore': [25, 94, 57, 62, 70, 25, 94, 57, 62, 70, 62, 70]}
+
+df = pd.DataFrame(raw_data)
+print(Entropia(df))
