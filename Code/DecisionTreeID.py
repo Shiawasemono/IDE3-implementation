a2324960c70c17c69494a4f1b7564157becdb9cd
Shiawasemono <nono.amps@gmail.com>
2018-10-25 16:10:31 +0200

    Quitado main de DecisionTreeID

--- Code/DecisionTreeID.py
+++ Code/DecisionTreeID.py
@@ -50,17 +50,6 @@ def ganancia(matriz_datos,col):
 	G = enGen + Sum
 	return G
 
-
-
-raw_data = {'regiment': ['Nighthawks', 'Nighthawks', 'Nighthawks', 'Nighthawks', 'Dragoons', 'Dragoons', 'Dragoons', 'Dragoons', 'Scouts', 'Scouts', 'Scouts', 'Scouts'], 
-        'company': ['1st', '1st', '2nd', '2nd', '1st', '1st', '2nd', '2nd','1st', '1st', '2nd', '2nd'], 
-        'name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze', 'Jacon', 'Ryaner', 'Sone', 'Sloan', 'Piger', 'Riani', 'Ali'], 
-        'preTestScore': [4, 24, 31, 2, 3, 4, 24, 31, 2, 3, 2, 3],
-        'postTestScore': [25, 94, 57, 62, 70, 25, 94, 57, 62, 70, 62, 70]}
-
-df = pd.DataFrame(raw_data)
-print(ganancia(df, col='regiment'))
-
 def DecisionTreeID(nombre_fichero):
     matriz_datos = pd.read_csv(nombre_fichero)
     matriz_datos_original = pd.read_csv(nombre_fichero) # Realmente necesario? Demanding?
